925d9575f3a97c61246f205fc3483289a71d8f71
usuijuice <jumpeisoccerguitar@gmail.com>
2025-10-20 00:52:13 +0900

    feat: add test text to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { lusitana } from "@/app/ui/fonts";
 export default function Page() {
 	return (
 		<main className="flex min-h-screen flex-col p-6">
+			テスト
 			<div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
 				<AcmeLogo />
 			</div>
