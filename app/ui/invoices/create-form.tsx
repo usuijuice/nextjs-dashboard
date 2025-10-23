53d06254c9d6830d9f1a1695f8651130ac0ff3b9
usuijuice <jumpeisoccerguitar@gmail.com>
2025-10-23 21:32:07 +0900

    feat: add create and edit invoice pages with form handling and data fetching

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -5,12 +5,13 @@ import {
 	UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { createInvoice } from "@/app/lib/actions";
 import type { CustomerField } from "@/app/lib/definitions";
 import { Button } from "@/app/ui/button";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
 	return (
-		<form>
+		<form action={createInvoice}>
 			<div className="rounded-md bg-gray-50 p-4 md:p-6">
 				{/* Customer Name */}
 				<div className="mb-4">
