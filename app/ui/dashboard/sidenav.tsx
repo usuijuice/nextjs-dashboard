3b8bc505119bf6eddaec860076cef560e6f0b30f
usuijuice <jumpeisoccerguitar@gmail.com>
2025-10-25 00:06:18 +0900

    fix: adjust button and input padding for consistent styling across forms and navigation

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -25,7 +25,7 @@ export default function SideNav() {
 					}}
 				>
 					<button
-						className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+						className="flex h-12 w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
 						type="submit"
 					>
 						<PowerIcon className="w-6" />
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -83,7 +83,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 					<legend className="mb-2 block text-sm font-medium">
 						Set the invoice status
 					</legend>
-					<div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+					<div className="rounded-md border border-gray-200 bg-white px-3.5 py-3">
 						<div className="flex gap-4">
 							<div className="flex items-center">
 								<input
