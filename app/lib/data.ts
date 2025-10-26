995b45cce9ee64de7bdf894a35afb38022466da7
usuijuice <jumpeisoccerguitar@gmail.com>
2025-10-26 10:52:45 +0900

    refactor: remove CustomersTable and related customer fetching logic

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,6 @@
 import postgres from "postgres";
 import type {
 	CustomerField,
-	CustomersTableType,
 	InvoiceForm,
 	InvoicesTable,
 	LatestInvoiceRaw,
@@ -173,38 +172,5 @@ export async function fetchCustomers() {
 	} catch (err) {
 		console.error("Database Error:", err);
 		throw new Error("Failed to fetch all customers.");
-	}
-}
-
-export async function fetchFilteredCustomers(query: string) {
-	try {
-		const data = await sql<CustomersTableType[]>`
-		SELECT
-		  customers.id,
-		  customers.name,
-		  customers.email,
-		  customers.image_url,
-		  COUNT(invoices.id) AS total_invoices,
-		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-		FROM customers
-		LEFT JOIN invoices ON customers.id = invoices.customer_id
-		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
-	  `;
-
-		const customers = data.map((customer) => ({
-			...customer,
-			total_pending: formatCurrency(customer.total_pending),
-			total_paid: formatCurrency(customer.total_paid),
-		}));
-
-		return customers;
-	} catch (err) {
-		console.error("Database Error:", err);
-		throw new Error("Failed to fetch customer table.");
 	}
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -55,26 +55,6 @@ export type InvoicesTable = {
 	status: "pending" | "paid";
 };
 
-export type CustomersTableType = {
-	id: string;
-	name: string;
-	email: string;
-	image_url: string;
-	total_invoices: number;
-	total_pending: number;
-	total_paid: number;
-};
-
-export type FormattedCustomersTable = {
-	id: string;
-	name: string;
-	email: string;
-	image_url: string;
-	total_invoices: number;
-	total_pending: string;
-	total_paid: string;
-};
-
 export type CustomerField = {
 	id: string;
 	name: string;
